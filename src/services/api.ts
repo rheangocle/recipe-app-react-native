c92d2e789187ffebc19814b99b8e939674eff99a
Rhea Le <rheangocle@gmail.com>
2025-02-16 18:02:48 -0500

    initial commit, basic registration page

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const BASE_API_URL = 'http://localhost:8000/api';
-
+// const BASE_API_URL = 'http://localhost:8000/api';
+const BASE_API_URL = 'http://127.0.0.1:8000/api'; // For Android emulator
 
 export async function registerUser(
     username: string,
     email: string,
     password: string,
 ): Promise<any> {
-    const response = await axios.post(`{BASE_API_URL}/register/`, {
+    const response = await axios.post(`${BASE_API_URL}/register/`, {
         username,
         email,
         password
